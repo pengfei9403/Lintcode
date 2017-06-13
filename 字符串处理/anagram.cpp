e62d5a445931fe423cbf8f2e9dc92ae2b766bdbe
pengfei9403 <pengfei9403@163.com>
2017-06-13 22:24:52 +0800

    Revert "delete one old cpp"
    This reverts commit 95a468bc1758009e7c3cb690cf4175a29219d3c8.

--- 字符串处理/anagram.cpp
+++ 字符串处理/anagram.cpp
@@ -3,21 +3,18 @@
 
 using namespace std;
 
-class Solution {
-	public:
-	    /**
-	     * @param s: The first string
-	     * @param b: The second string
-	     * @return true or false
-	     */
-	    bool anagram(string s, string t) {
-	        // write your code here
-	        sort(s.begin(),s.end());
-	        sort(t.begin(),t.end());
-	        if(s == t){
-	            return true;
-	        }else{
-	            return false;
-	        }
-	    }
-};
+/**
+ * @param s: The first string
+ * @param b: The second string
+ * @return true or false
+ */
+bool anagram(string s, string t) {
+    // write your code here
+    sort(s.begin(),s.end());
+    sort(t.begin(),t.end());
+    if(s == t){
+        return true;
+    }else{
+        return false;
+    }
+}
--- 字符串处理/compareStrings.cpp
+++ 字符串处理/compareStrings.cpp
@@ -3,37 +3,34 @@
 
 using namespace std;
 
-class Solution {
-	public:
-	    /**
-	     * @param A: A string includes Upper Case letters
-	     * @param B: A string includes Upper Case letter
-	     * @return:  if string A contains all of the characters in B return true
-	     *           else return false
-	     */
-	    bool compareStrings(string A, string B) {
-	        // write your code here
-	        int flag = 0 ;
-	        int i = 0 , j = 0 ;
-	        if(0 == B.size()){
-	            return true ;
-	        }
-	        sort(A.begin(),A.end());
-	        sort(B.begin(),B.end());
-	        for(; j < B.size() && i < A.size() ;){
-	            if(A[i] != B[j]){
-	                flag = 0 ;
-	                i++ ;
-	            }else{
-	                flag = 1 ;
-	                i++ ;
-	                j++ ;
-	            }
-	        }
-	        if(flag && j == B.size()){
-	            return true ;
-	        }else{
-	            return false ;
-	        }
-	    }
-};
+/**
+ * @param A: A string includes Upper Case letters
+ * @param B: A string includes Upper Case letter
+ * @return:  if string A contains all of the characters in B return true
+ *           else return false
+ */
+bool compareStrings(string A, string B) {
+    // write your code here
+    int flag = 0 ;
+    int i = 0 , j = 0 ;
+    if(0 == B.size()){
+        return true ;
+    }
+    sort(A.begin(),A.end());
+    sort(B.begin(),B.end());
+    for(; j < B.size() && i < A.size() ;){
+        if(A[i] != B[j]){
+            flag = 0 ;
+            i++ ;
+        }else{
+            flag = 1 ;
+            i++ ;
+            j++ ;
+        }
+    }
+    if(flag && j == B.size()){
+        return true ;
+    }else{
+        return false ;
+    }
+}
